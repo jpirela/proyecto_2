9b50e7ca3b47538a1350ae256d16077ffb4f0c61
Manudev01 <bymadby80@gmail.com>
2025-06-24 15:26:10 -0400

    Organizacion de directorios y creacion de la base para el backend

---
+++ backend/src/main/java/com/todolist/Manudev01/urbe/TodoListManuelDiazApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.Manudev01.urbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListManuelDiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListManuelDiazApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/todolist/Manudev01/urbe/TodoListManuelDiazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.Manudev01.urbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListManuelDiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
